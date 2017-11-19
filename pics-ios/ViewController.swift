265c7cf286cd0395f410dff13992be3091bb6a1a
Michael Skogberg <michael.skogberg@futurice.com>
2017-11-19 11:40:50 +0200

    Initial Commit

---
+++ pics-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pics-ios
+//
+//  Created by Michael Skogberg on 19/11/2017.
+//  Copyright © 2017 Michael Skogberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
